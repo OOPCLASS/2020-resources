ef1ba7546b004c6fabed0d5d1629fb07e6bf67a9
Alex Bondor <alexandru.viorel.bondor@gmail.com>
2020-10-05 19:29:39 +0300

    Added lab1

---
+++ lab1/src/ro/utcluj/Main.java
@@ -0,0 +1,46 @@
+package ro.utcluj;
+
+import java.util.Date;
+
+public class Main {
+
+    private static final long ONE_DAY_IN_MS = 86400000;
+
+    public static void main(String[] args) {
+        // write your code here
+        int numberOfBranches = 10;
+    }
+
+    public static long getElapsedTimeInDaysSince(long timestampInMs) {
+        return getElapsedTimeInMsSince(timestampInMs) / ONE_DAY_IN_MS;
+    }
+
+    /**
+     * @param timestampInMs
+     * @return
+     */
+    public static long getElapsedTimeInMsSince(long timestampInMs) {
+        long now = new Date().getTime();
+//    return now - timestampInMs;
+        boolean flag = false;
+        Boolean flaaag = null;
+
+        if (flag) {
+            return 0L;
+        }
+
+        int a = 10;
+
+        return switch (a) {
+            case 10 -> 10L;
+            case 100 -> 100L;
+            default -> 0L;
+        };
+
+//    return switch (a) {
+//      case 10 -> 10L;
+//      case 100 -> 100L;
+//      default -> -1L;
+//    };
+    }
+}
